b54bd8390cbd078eafb612f0b7b0aee3dfe4277c
gabriel <gabmt2210@gmail.com>
2023-04-07 19:41:53 -0300

    exercicios 3 e 4

--- checkpoint.js
+++ checkpoint.js
@@ -1,3 +1,35 @@
+//Exercício 3
+let pesoa = prompt("quanto vc pesa?")
+let alturaa = prompt("qual sua altura (em metros, ex 1.70)?")
+let pesob = parseFloat(pesoa)
+let alturab = parseFloat(alturaa)
+
+let imc = pesob / (alturab*alturab)
+
+if(imc<18.5){
+document.write("você esta abaixo do peso");
+}else if(imc>=18.5 && imc<=24.9){
+document.write("você esta no peso ideal ");
+}else{
+document.write("você esta acima do peso");
+}
+
+document.write("<br><br>");
+
+//Exercício 4
+let idade = prompt("qual sua idade?")
+if(idade<=12){
+document.write("criança")
+}else if(idade>=13 && idade<=18){
+document.write("adolescente")
+}else if(idade<=60){
+document.write("adulto")
+}else{
+document.write("idoso")
+}
+
+document.write("<br><br>");
+
 //Exercício 5
 let user="admin"
 let password="1234"
@@ -16,3 +48,5 @@ alert("Login realizado com sucesso!");
 let txt="O LUGAR VIRA TECNOLOGIA"
 document.write(txt + "<br>");
 document.write(txt.replace("LUGAR","MUNDO"));
+
+document.write("<br><br>");
